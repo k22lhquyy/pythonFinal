3af646825f6a14820f4b860a75902d2f951e37e7
quyle <huuquyle9@gmail.com>
2024-11-14 23:41:15 +0700

    buoi2

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
-Gaming = 'database.sqlite3'
+DB_NAME = 'database.sqlite3'
 
 
 def create_database():
@@ -14,13 +15,22 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'abcdefghijklmnopqrstuvwxyzABCDEF'
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{Gaming}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    @login_manager.user_loader
+    def load_user(id):
+        return Customer.query.get(int(id))
 
     from .views import views
     from .auth import auth
     from .admin import admin
+    from  .models import Customer, Cart, Product, Order
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
